4273a56ad2e5436e985bb496f1f9a64a7c3bb54e
Zadkiel <hello@zadkiel.fr>
2020-11-01 14:17:25 +0000

    initial commit

--- api/query.js
+++ api/query.js
@@ -1,7 +1,38 @@
-module.exports = (req, res) => {
-  res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
+const fetch = require('node-fetch')
+const parse = require('csv-parse/lib/sync')
+
+const fetchAndFilter = async (stop, direction) => {
+  const res = await fetch("http://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_TpsReel.csv")
+  const tamCSV = await res.text()
+
+  const records = parse(tamCSV, {
+    columns: true,
+    skip_empty_lines: true,
+    delimiter: ';',
+  })
+
+  return records.filter(r => {
+    return r.stop_name == stop.toUpperCase() &&
+      r.trip_headsign == direction.toUpperCase() 
   })
 }
+
+module.exports = async (req, res) => {
+  try {
+    const result = await fetchAndFilter(req.query.stop, req.query.direction)
+  } catch (error) {
+    res.json({
+      success: false,
+      error: error,
+    })
+  }
+
+  res.json({
+    success: true,
+    result: result,
+  })
+}
+
+// (async () => {
+// 	console.log(await fetchAndFilter("ANTIGONE", "MOSSON"))
+// })();
